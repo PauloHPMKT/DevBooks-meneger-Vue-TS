bb5a7e5c5678270a39cfa5db8c8b1134f335d944
Paulo Sergio <pauloserg861@gmail.com>
2022-11-23 21:39:47 -0300

    criando estilizacao e estruturando pagina de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,23 @@
 <template>
-	<div>
-		Login
+	<div class="main-banner">
+		<main>
+      <div class="box-content">
+        <div class="form-container">
+          <div class="image-container">
+            <img src="/img/logo-livraria-do-dev-removebg.png" alt="logo principal livraria do dev">
+          </div>
+          <div class="title-content">
+            <h1>Gerenciador de plataforma</h1>
+            <p>Acesse seu ambiente de gestão da plataforma DevBooks</p>
+          </div>
+          <!--<form-login @handleLogin="submitLogin"/>
+          <information-popup
+            v-if="toastMessageHidden"
+            :message_data="message"
+           />-->
+        </div>
+      </div>
+    </main>
 	</div>
 </template>
 
