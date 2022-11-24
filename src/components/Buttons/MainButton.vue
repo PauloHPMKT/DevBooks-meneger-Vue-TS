deb1b70da51b0c9ec50ec8471e5b30650b09e4fb
Paulo Sergio <pauloserg861@gmail.com>
2022-11-23 22:07:01 -0300

    criando componente de botao

---
+++ src/components/Buttons/MainButton.vue
@@ -0,0 +1,18 @@
+<template>
+	<button class="btn">
+		{{ title_btn }}
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'MainButton',
+	props: {
+		title_btn: {
+			type: String,
+		},
+	},
+})
+</script>
---
+++ src/components/Forms/FormLogin.vue
@@ -0,0 +1,28 @@
+<template>
+	<form @submit.prevent="teste">
+		<label for="email">E-mail</label>
+		<div class="input-style">
+			<input id="email" type="text" placeholder="meuemail@devbooks.com" />
+		</div>
+		<label for="password">Senha</label>
+		<div class="input-style">
+			<input id="email" type="password" placeholder="**********" />
+		</div>
+		<MainButton class="btn-style-success" :title_btn="'Login'"/>
+	</form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MainButton from "../Buttons/MainButton.vue";
+
+export default defineComponent({
+	name: "FormLogin",
+	methods: {
+		teste() {
+			alert("ok");
+		},
+	},
+	components: { MainButton },
+});
+</script>
