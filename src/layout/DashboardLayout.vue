2619dba073878b89c589a8de0b1b2668218f9956
Paulo Sergio <pauloserg861@gmail.com>
2022-12-08 19:45:51 -0300

    implementando componentes

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -2,22 +2,25 @@
 	<div class="wrapper">
 		<SideBar />
 		<div class="main-painel">
-			<div>header</div>
+			<Header />
 			<div>
 				<Content />
 			</div>
-			<div>footer</div>
+			<Footer />
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import Content from "./Content.vue";
-import SideBar from "../components/SideBar/index.vue";
+import { defineComponent } from "vue"
+
+import SideBar from '@/components/SideBar/index.vue'
+import Header from '@/components/Header/index.vue'
+import Content from './Content.vue'
+import Footer from '@/components/Footer/index.vue'
 
 export default defineComponent({
 	name: "DashboardLayout",
-	components: { SideBar, Content },
+	components: { SideBar, Header, Content, Footer },
 });
 </script>
