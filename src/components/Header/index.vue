8997a8b811f4a991b6afb2b8d2347a0b86a214b6
Paulo Sergio <pauloserg861@gmail.com>
2022-12-08 22:22:13 -0300

    criando estrutura do header

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,7 +1,17 @@
 <template>
-	<div>
-		Cabecalho
-	</div>
+	<header class="header">
+		<div class="header__infos">
+			<div class="header--gretting">
+				Seja bem vindo <strong>Nome do Usuário</strong>
+			</div>
+			<div>
+				<strong>Dashboard</strong>
+			</div>
+		</div>
+		<div>
+			botoes de comando
+		</div>
+	</header>
 </template>
 
 <script lang="ts">
