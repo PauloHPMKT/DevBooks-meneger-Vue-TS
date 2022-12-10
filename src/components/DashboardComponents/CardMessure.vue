7e42ef1c2762d5d3a21ac5da88519e37248d37ed
Paulo Sergio <pauloserg861@gmail.com>
2022-12-09 21:27:57 -0300

    criando componente de card

---
+++ src/components/DashboardComponents/CardMessure.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="card">
+		<div class="card__information">
+			<p>{{ status }}</p>
+			<span class="card--type-font">{{ quantity }}</span>
+		</div>
+		<div class="card__icon">
+			<Icon :icon="`carbon:${ icon_type }`" />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Icon } from "@iconify/vue";
+
+export default defineComponent({
+	name: "CardMessure",
+	components: { Icon },
+	props: {
+		status: {
+			type: String,
+		},
+		icon_type: {
+			type: String,
+		},
+		quantity: {
+			type: Number,
+		}
+	}
+});
+</script>
