cbb02d49b9beef9b32043293ba7cb01f05c501fe
Paulo Sergio <pauloserg861@gmail.com>
2022-12-09 22:12:14 -0300

    implementando configuracoes

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/sass/main.scss'
+import { store } from './store'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.config.globalProperties.$store = store
+app.provide('$store', store)
 app.mount('#app')
