dda069bd37305b4539376a515a8896d68e559b0a
Paulo Sergio <pauloserg861@gmail.com>
2022-12-31 14:22:43 -0300

    criando interface dos campos do livro

---
+++ src/interfaces/Ibooks.ts
@@ -0,0 +1,19 @@
+export interface IBookFields {
+	cod: number;
+	poster: string;
+	title: string;
+	author: string;
+	publishing: string;
+	plot: string;
+	full_plot: string;
+	genres: string;
+	language: string;
+	pages_number: number;
+	technical: ITechnical;
+}
+
+interface ITechnical {
+	rating: number;
+	readers: number;
+	votes: number;
+}
