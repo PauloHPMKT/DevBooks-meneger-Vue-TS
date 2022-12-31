91bd8ad5f7463d0d82454c6b7fe6c9728e85bf07
Paulo Sergio <pauloserg861@gmail.com>
2022-12-31 19:55:49 -0300

    criando state para authors

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { createStore } from "vuex";
 import userStore from "./users/user-store";
 import bookStore from "./books/book-store";
+import authorStore from "./author/author-store";
 import createPersistedState from "vuex-persistedstate";
 
 export const store = createStore({
 	modules: {
 		userStore,
 		bookStore,
+		authorStore,
 	},
 	plugins: [createPersistedState()],
 });
