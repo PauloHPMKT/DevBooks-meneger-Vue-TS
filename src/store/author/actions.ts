4f6cdce0d21eabe597c36ff8fc56771cdc06f2b6
Paulo Sergio <pauloserg861@gmail.com>
2023-01-01 11:51:24 -0300

    consumindo dados dos autores

--- src/store/author/actions.ts
+++ src/store/author/actions.ts
@@ -3,9 +3,9 @@ import authorService from "@/services/authorService";
 export default {
 	async getAuthors({ commit }: any, data: object) {
 		await authorService.getAuthors().then((res) => {
-			console.log(res);
+			console.log(res.data);
 
-			commit("GET_AUTHORS", res);
+			commit("GET_AUTHORS", res.data);
 		});
 	},
 };
--- src/views/user/AuthorsReports.vue
+++ src/views/user/AuthorsReports.vue
@@ -1,9 +1,27 @@
 <template>
-	<div>Authores</div>
+	<div>{{ $store.state.authorStore.authors }}</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+	name: "AuthorsReports",
+	data() {
+		return {
+			authors: [],
+		};
+	},
+
+	methods: {
+		async getAllAuthors() {
+			await this.$store.dispatch("authorStore/getAuthors", this.authors);
+		},
+	},
+
+	mounted() {
+		this.getAllAuthors();
+		console.log(this.$store.state);
+	},
+});
 </script>
