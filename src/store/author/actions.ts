a0b29b54101829ed26403306e709cadddb6ba445
Paulo Sergio <pauloserg861@gmail.com>
2023-01-01 16:53:04 -0300

    criando actions para get de autores

--- src/store/author/actions.ts
+++ src/store/author/actions.ts
@@ -3,9 +3,9 @@ import authorService from "@/services/authorService";
 export default {
 	async getAuthors({ commit }: any, data: object) {
 		await authorService.getAuthors().then((res) => {
-			console.log(res.data);
+			console.log(res.data.author);
 
-			commit("GET_AUTHORS", res.data);
+			commit("GET_AUTHORS", res.data.author);
 		});
 	},
 };
