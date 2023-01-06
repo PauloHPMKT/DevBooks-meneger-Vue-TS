444f63e4e74a2afb4208014e8e60a6d280d5e445
Paulo Sergio <pauloserg861@gmail.com>
2023-01-06 18:52:29 -0300

    criando metodo de remove uploadImage

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,11 +1,15 @@
 import { ApiService } from "./api";
 
 export default {
-	getUploadedImages: (id: string) => {
+	/*getUploadedImages: (id: string) => {
 		return ApiService.get(`/books/upload/${id}`);
-	},
+	},*/
 
 	uploadImage: (data: object, headers: any) => {
 		return ApiService.post(`/books/upload`, data, headers);
+	},
+
+	removeUploaedImage: (id: string) => {
+		return ApiService.delete(`/books/upload/${id}`);
 	},
 };
