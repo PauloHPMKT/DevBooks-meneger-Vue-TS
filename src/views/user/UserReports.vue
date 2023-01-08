a2ecd7cb1c8b69596d40076f099e9237bb40e911
Paulo Sergio <pauloserg861@gmail.com>
2023-01-08 17:24:01 -0300

    adicionando metodo da store

--- src/views/user/UserReports.vue
+++ src/views/user/UserReports.vue
@@ -4,18 +4,38 @@
 			:search_name="'Encontre um usuário'"
 			:event_name="'Cadastrar novo usuário'"
 		/>
-		<ul class="table"></ul>
+		<ul class="table">
+			<CardUserList :user_state="users" />
+		</ul>
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import SearchBar from "@/components/SearchBar/index.vue";
+import CardUserList from "@/components/Cards/User/CardUserList.vue";
 
 export default defineComponent({
 	name: "UserReports",
 	components: {
 		SearchBar,
+		CardUserList,
+	},
+
+	data() {
+		return {
+			users: this.$store.state.userStore.Users,
+		};
+	},
+
+	methods: {
+		async getAllUsers() {
+			await this.$store.dispatch("userStore/getAllUsers", this.users);
+		},
+	},
+
+	mounted() {
+		this.getAllUsers();
 	},
 });
 </script>
