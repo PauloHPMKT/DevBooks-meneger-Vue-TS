e13a609781635d2568c826a442a043b81db9dcc8
Paulo Sergio <pauloserg861@gmail.com>
2023-01-11 14:12:58 -0300

    criando card  de exibicao do ultimo livro

--- src/views/user/Overview.vue
+++ src/views/user/Overview.vue
@@ -22,22 +22,60 @@
 				:quantity="305"
 			/>
 		</header>
-		<div></div>
+		<div class="table overview_table">
+			<section>
+				<div class="size-container">conteudo 1</div>
+				<div class="size-container">conteudo 2</div>
+				<div class="size-container book-card">
+					<div class="cover">
+						<img
+							v-if="
+								bookIndex[0].poster.image_cover === ''
+									? empty.poster
+									: bookIndex[0].poster.image_cover
+							"
+							:src="
+								`${imagePath}/${bookIndex[0].poster.image_cover}` ||
+								empty.poster
+							"
+							:alt="bookIndex[0].title"
+						/>
+					</div>
+					<div class="info-book">
+						<h2>Ultimo livro adicionado ao DevBooks</h2>
+						<h3>
+							{{ bookIndex[0].title }}
+						</h3>
+						<p>
+							{{ bookIndex[0].plot }}
+						</p>
+						<span>Ver mais <Icon icon="carbon:arrow-right" /></span>
+					</div>
+				</div>
+			</section>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import CardMessure from "@/components/DashboardComponents/CardMessure.vue";
 import { defineComponent } from "vue";
+import CardMessure from "@/components/DashboardComponents/CardMessure.vue";
+import { Icon } from "@iconify/vue";
+const HOST_URI = import.meta.env.VITE_HOST_URI;
 
 export default defineComponent({
 	name: "Overview",
-	components: { CardMessure },
+	components: { CardMessure, Icon },
 	data() {
 		return {
+			imagePath: HOST_URI,
+			empty: {
+				poster: "img/empty-cover.png",
+			},
 			quantityUser: this.$store.state.userStore.countUsers,
 			quantityBooks: 476,
 			quantityComments: 851,
+			bookIndex: this.$store.state.bookStore.Books,
 		};
 	},
 });
