423f11b9e94a2c863b51c65e6ba095dd7f5f5e54
Paulo Sergio <pauloserg861@gmail.com>
2023-01-10 22:19:28 -0300

    implementando paginacao

--- src/views/user/UserReports.vue
+++ src/views/user/UserReports.vue
@@ -7,6 +7,14 @@
 		<ul class="table">
 			<CardUserList :user_state="users" />
 		</ul>
+		<div>
+			<Pagination
+				:totalPages="10"
+				:perPage="10"
+				:currentPage="currentPage"
+				@pagechanged="onPageChange"
+			/>
+		</div>
 	</div>
 </template>
 
@@ -14,23 +22,32 @@
 import { defineComponent } from "vue";
 import SearchBar from "@/components/SearchBar/index.vue";
 import CardUserList from "@/components/Cards/User/CardUserList.vue";
+import Pagination from "@/components/Pagination/Pagination.vue";
 
 export default defineComponent({
 	name: "UserReports",
 	components: {
 		SearchBar,
 		CardUserList,
+		Pagination,
 	},
 
 	data() {
 		return {
 			users: this.$store.state.userStore.Users,
+			currentPage: 1,
 		};
 	},
 
 	methods: {
 		async getAllUsers() {
-			await this.$store.dispatch("userStore/getAllUsers", this.users);
+			await this.$store.dispatch("userStore/getAllPaginatedUsers", this.users);
+		},
+		// criar url paginada
+		onPageChange(page: number) {
+			console.log(page);
+
+			this.currentPage = page;
 		},
 	},
 
