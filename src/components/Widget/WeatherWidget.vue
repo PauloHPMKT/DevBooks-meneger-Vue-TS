718b4889956c9ca810f8ccfd7cf1b768793320e7
Paulo Sergio <pauloserg861@gmail.com>
2023-01-11 22:21:18 -0300

    criando componente de widget

---
+++ src/components/Widget/WeatherWidget.vue
@@ -0,0 +1,104 @@
+<template>
+	<div
+		class="tag"
+		:class="{
+			nightActive: nightStage,
+			afternoonActive: afternoonStage,
+			dayActive: dayStage,
+		}"
+	>
+		<span>{{ day_period }}</span>
+		<div class="date_time">{{ date_time }}</div>
+		<div class="weather">
+			<span>28 °C</span>
+			<p>Maranguape</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import weatherApi from "../../services/weatherApi";
+
+export default defineComponent({
+	name: "WeatherWidget",
+	data() {
+		return {
+			day_period: "",
+			date_time: "",
+			nightStage: false,
+			afternoonStage: false,
+			dayStage: false,
+		};
+	},
+	methods: {
+		async weatherRequest() {
+			await weatherApi.getWeatherResults().then((res) => {
+				console.log(res);
+			});
+		},
+
+		clockTimer() {
+			setInterval(() => {
+				const data = new Date();
+				const currentTime = [
+					data.getHours(),
+					data.getMinutes(),
+					data.getSeconds(),
+				]
+					.map((valor) => `0${valor}`.slice(-2))
+					.join(":");
+
+				this.date_time = currentTime;
+
+				if (currentTime >= "18:00:00" && currentTime <= "23:59:59") {
+					this.day_period = "Boa Noite";
+					this.nightStage = true;
+				} else if (currentTime >= "00:00:00" && currentTime <= "11:59:59") {
+					this.day_period = "Bom dia";
+					this.dayStage = true;
+				} else {
+					return (this.day_period = "Boa tarde"), (this.afternoonStage = true);
+				}
+			}, 1000);
+		},
+	},
+	beforeMount() {
+		this.clockTimer();
+		//this.weatherRequest();
+	},
+});
+</script>
+
+<style scoped lang="scss">
+@import url(https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;800;900&display=swap);
+
+.tag {
+	width: 40%;
+	margin-left: 25px;
+	padding: 20px;
+	border-radius: 8px;
+	color: #fff;
+	.date_time {
+		font-family: "Roboto", sans-serif;
+		font-size: 30px;
+	}
+
+	.weather {
+		margin-top: 50px;
+		span {
+			font-size: 35px;
+		}
+	}
+}
+
+.dayActive {
+	background-image: url("/img/morning-sky.jpg");
+}
+.afternoonActive {
+	background-image: url("/img/afternoon.jpg");
+}
+.nightActive {
+	background-image: url("/img/night-bg.webp");
+}
+</style>
